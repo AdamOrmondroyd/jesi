3e1ba49763885daf7ca31f96360fd6a9fd12e653
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-07 18:16:47 +0100

    split anesthetic logZ over two lines

--- nested_sampling.py
+++ nested_sampling.py
@@ -68,7 +68,8 @@ def save(final, filename, labels, flatten=None):
     )
 
     samples.to_csv(f"{filename}.csv")
-    print(f"anesthetic logZ = {samples.logZ():.2f} = {samples.logL_P():.2f} - {samples.D_KL():.2f}")
+    print(f"anesthetic logZ = {samples.logZ():.2f} "
+          f"= {samples.logL_P():.2f} - {samples.D_KL():.2f}")
     return samples, final
 
 
