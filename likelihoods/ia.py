8f80c001824ad016fa192cebd556b8d28977b47a
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-11 15:28:25 +0100

    do prep in numpy

--- likelihoods/ia.py
+++ likelihoods/ia.py
@@ -1,33 +1,9 @@
-from jax.numpy import array, eye, log, log10, ones, pi
-from jax.numpy.linalg import solve, cholesky
+import numpy as np
+from jax.numpy import array, log10
 from scipy.constants import c
 
 
 c = c / 1000.0
-
-
-def fast_constrained_quadratic_form(L_inv, y, v, v_dot_v):
-    """
-    Compute -y.T @ C^-1_tilde @ y using precomputed L^-1.
-
-    Args:
-        L_inv: precomputed L^-1 matrix
-        y: data vector
-        v: precomputed L^-1 @ one
-        v_dot_v: precomputed v.T @ v
-
-    Returns:
-        Quadratic form value computed fast
-    """
-    # Fast matrix multiplication u = L^-1 @ y
-    u = L_inv @ y
-
-    # Compute the constrained quadratic form
-    u_dot_u = (u.T @ u).squeeze()
-    u_dot_v = (u.T @ v).squeeze()
-
-    # Constrained quadratic form: -y.T @ C^-1_tilde @ y
-    return -(u_dot_u - u_dot_v**2 / v_dot_v)
 
 
 class IaLogL:
@@ -41,27 +17,26 @@ class IaLogL:
         self.zhd = array(df['zHD'].to_numpy()[mask])
         self.zhel = array(df['zHEL'].to_numpy()[mask])
 
-        self.cov = array(cov[mask, :][:, mask])
+        # Do all matrix operations in fp64 numpy for precision
+        cov_np = np.array(cov[mask, :][:, mask], dtype=np.float64)
+        one_np = np.ones((len(cov_np), 1), dtype=np.float64)
 
-        # Use Cholesky decomposition for numerical stability
-        self.L = cholesky(self.cov)
-        self.one = ones(len(self.cov))[:, None]
+        # Compute constrained inverse C^-1_tilde in fp64
+        # This marginalizes out nuisance parameters (H0, absolute magnitude)
+        invcov_np = np.linalg.inv(cov_np)
+        invcov_one = invcov_np @ one_np
+        one_T_invcov_one = (one_np.T @ invcov_one).squeeze()
 
-        # Precompute L^-1 for fast matrix multiplication
-        self.L_inv = solve(self.L, eye(len(self.cov)))
-        
-        # Precompute v = L^-1 @ one (constant for all likelihood evaluations)
-        self.v = self.L_inv @ self.one
-        self.v_dot_v = (self.v.T @ self.v).squeeze()
-        
-        # Compute normalization stably using Cholesky factor
-        one_T_invcov_one = self.v_dot_v
+        # Constrained inverse: C^-1_tilde = C^-1 - (C^-1 @ 1)(1^T @ C^-1) / (1^T @ C^-1 @ 1)
+        self.invcov_tilde = array(
+            invcov_np - (invcov_one @ invcov_one.T) / one_T_invcov_one)
 
-        # Log normalization: -0.5 * [log|2πC| + log(1^T C^-1 1)]
+        # Compute log normalization in fp64
+        sign, logdet = np.linalg.slogdet(cov_np)
         self.lognormalisation = -0.5 * (
-            2 * log(self.L.diagonal()).sum()  # log|C| from Cholesky
-            + log(2*pi) * len(self.cov)      # log(2π)^n
-            + log(one_T_invcov_one)          # log(1^T C^-1 1)
+            logdet                           # log|C|
+            + np.log(2*np.pi) * len(cov_np)  # log(2π)^n
+            + np.log(one_T_invcov_one)       # log(1^T C^-1 1)
         )
 
     def _y(self, params, cosmology):
@@ -71,8 +46,8 @@ class IaLogL:
     def __call__(self, params, cosmology):
         y = self._y(params, cosmology)
 
-        # Compute constrained quadratic form fast (L_inv, v, v_dot_v precomputed)
-        quadratic_form = fast_constrained_quadratic_form(self.L_inv, y, self.v, self.v_dot_v)
+        # Fast quadratic form with precisely computed constrained inverse
+        quadratic_form = -y.T @ self.invcov_tilde @ y
 
         result = quadratic_form / 2 + self.lognormalisation
         return result.squeeze()
