5c93e01a2584c260aa82df751ab6d88e2c853994
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-14 09:33:24 +0100

    remove kahan summation

--- cosmology/cosmology.py
+++ cosmology/cosmology.py
@@ -6,20 +6,6 @@ from scipy.constants import c
 c = c/1000
 
 
-def kahan_cumsum(x):
-    """Kahan summation algorithm for improved numerical precision."""
-    def body_fun(carry, xi):
-        s, c = carry
-        y = xi - c
-        t = s + y
-        c = (t - s) - y
-        return (t, c), t
-
-    init_carry = (x[0], 0.0)
-    _, result = jax.lax.scan(body_fun, init_carry, x[1:])
-    return concatenate([x[:1], result])
-
-
 @jax.jit
 def cumulative_trapezoid(y, x):
     dx = diff(x, axis=-1)
