53313bb6f67ea52e6405d6f2b43101101151b65b
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-07-25 10:52:34 +0100

    sort out more packaging stuff

--- jayesian/__init__.py
+++ jayesian/__init__.py
@@ -1,2 +1,11 @@
+from jayesian._version import __version__
 from jayesian import cosmology
 from jayesian import likelihoods
+from jayesian import nested_sampling
+
+__all__ = [
+    "__version__",
+    "cosmology",
+    "likelihoods",
+    "nested_sampling",
+]
