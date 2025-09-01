2a0c2b1cd99789ba3779af3f701dd9235a20d708
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2025-09-01 13:26:53 +0100

    add offset des5y likelihood

--- jayesian/likelihoods/des5y.py
+++ jayesian/likelihoods/des5y.py
@@ -1,4 +1,5 @@
 from numpy import loadtxt, argsort, sqrt, fill_diagonal
+from jax.numpy import array
 import pandas as pd
 from pathlib import Path
 from jayesian.likelihoods.ia import IaLogL, IaLogLUnmarginalised
@@ -18,3 +19,24 @@ df = df.iloc[idx]
 logl = IaLogL(df, cov, 'MU')
 
 loglunmarginalised = IaLogLUnmarginalised(df, cov, 'MU')
+
+# george fiddle
+des_id = 10.0
+george_mask = df['IDSURVEY'] != des_id
+
+
+class GeorgeIaLogL(IaLogL):
+    requirements = {'h0_dl_over_c', 'delta_mb'}
+
+    def __init__(self, george_mask, *args, **kwargs):
+        self.george_mask = array(george_mask.to_numpy())
+        super().__init__(*args, **kwargs)
+
+    def _y(self, params, cosmology):
+        # offset is the george offset
+        y = super()._y(params, cosmology)
+        y -= params['delta_mb'] * self.george_mask
+        return y
+
+
+logloffset = GeorgeIaLogL(george_mask, df, cov, mb_column='MU', z_cutoff=0.0)
