f8a12888342b2936e1228f5970a42895e990233e
Adam Ormondroyd <52655393+AdamOrmondroyd@users.noreply.github.com>
2025-09-01 13:28:25 +0100

    add des5yoffset (#3)
    * add offset des5y likelihood
    
    * add delta_mb to set of priors

--- jayesian/likelihoods/__init__.py
+++ jayesian/likelihoods/__init__.py
@@ -6,10 +6,11 @@ from jayesian.likelihoods.pantheonplus import (
 from jayesian.likelihoods.des5y import (
     logl as des5y,
     loglunmarginalised as des5yunmarginalised,
+    logloffset as des5yoffset
 )
 
 __all__ = [
     "desidr2",
     "pantheonplus", "pantheonplusunmarginalised",
-    "des5y", "des5yunmarginalised",
+    "des5y", "des5yunmarginalised", "des5yoffset",
 ]
