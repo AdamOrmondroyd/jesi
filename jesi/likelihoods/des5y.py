1de8d1400e09747a9d42f2947e2f9689d3615873
AdamOrmondroyd <Adam.Ormondroyd@gmail.com>
2026-01-21 16:40:30 +0000

    unused import

--- jesi/likelihoods/des5y.py
+++ jesi/likelihoods/des5y.py
@@ -2,7 +2,7 @@ from numpy import loadtxt, argsort, sqrt, fill_diagonal
 from jax.numpy import array
 import pandas as pd
 from pathlib import Path
-from jesi.likelihoods.ia import IaLogL, IaLogLUnmarginalised
+from jesi.likelihoods.ia import IaLogL
 
 
 # data loading stolen from Toby
